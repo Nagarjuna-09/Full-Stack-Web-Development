bd6454e50e3a2055c72b1c89463b7a045a7e7416
Nagarjuna-09 <nagarjunanathani434@gmail.com>
2023-04-17 07:23:27 -0400

    updated open weather api

--- 12. Weather API/server.js
+++ 12. Weather API/server.js
@@ -39,30 +39,3 @@ app.post('/', function(req,res){
         })
     })
 })
-
-
-
-
-// app.get('/', function(req,res){
-//     const url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=c876aedb46c565b6fe94baacc9594532&units=metric";
-//     //const url = "https://api.openweathermap.org/data/2.5/weather?lat=42.104800&lon=-75.932390&appid=c876aedb46c565b6fe94baacc9594532&units=metric";
-    
-//     https.get(url,function(response){
-//         console.log(response.statusCode);
-
-//         response.on('data', function(data) {
-//             const weatherData = JSON.parse(data);
-//             console.log(weatherData);
-//             const name = weatherData.name;
-//             const temp = weatherData.main.temp;
-//             const icon_code = weatherData.weather[0].icon; 
-//             const weatherDescription = weatherData.weather[0].description;
-//             res.write("<p>The weather is currently: "+weatherDescription+"</p>");
-//             res.write("<h1>The current temperature in "+ name+ " is: "+temp + " degrees Celcius</h1>");
-//             const image_url = "https://openweathermap.org/img/wn/"+icon_code+"@2x.png";
-//             res.write("<img src="+image_url+">");
-//             res.send();
-//         })
-//     })
-// })
-
