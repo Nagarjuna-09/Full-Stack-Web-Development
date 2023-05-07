3980608db2ba17023e50457cc45b4f2e74d8a7e5
Nagarjuna-09 <nagarjunanathani434@gmail.com>
2023-05-07 14:56:40 -0400

    implemented using bcrypt

--- 19. Authentication and Security/app.js
+++ 19. Authentication and Security/app.js
@@ -3,11 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+
 const app = express();
-var encrypt = require('mongoose-encryption');
+//var encrypt = require('mongoose-encryption');
+//var md5 = require('md5');
+const bcrypt = require('bcrypt');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+const saltRounds = 12;
 
 //connect to local mongodb database on your laptop. You can access this via mongoshell (using mongosh command in cmd) or ROBO 3T SW from laptop
 mongoose.connect('mongodb://127.0.0.1:27017/userDB')
@@ -18,7 +23,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 
 const users = new mongoose.model("user", userSchema);
@@ -36,26 +41,33 @@ app.get('/register', function (req, res) {
 });
 
 app.post('/register', function (req, res) {
-    const new_user = new users({
-        email: req.body.username,
-        password: req.body.password
-    });
-    new_user.save().then(()=>{
-        res.render('secrets');
+    bcrypt.hash(req.body.password, saltRounds).then(function (hash) {
+        // Store hash in your password DB.
+        const new_user = new users({
+            email: req.body.username,
+            password: hash
+        });
+        new_user.save().then(() => {
+            res.render('secrets');
+        }).catch((err) => {
+            console.log(err);
+        }); 
     }).catch((err) => {
         console.log(err);
-    }); 
+    });
 });
 
 app.post('/login', function (req,res) {
     users.findOne({email: req.body.username}).then((founduser)=>{
         if (founduser){
-            if (founduser.password === req.body.password) {
-                res.render('secrets');
-            }
-            else{
-                res.send('Incorrect Password');
-            }
+            bcrypt.compare(req.body.password, founduser.password).then(function (result) {
+                if (result) {
+                    res.render('secrets');
+                }
+                else {
+                    res.send('Incorrect Password');
+                }
+            });       
         }
         else{
             res.send('User not found. Please register or login with correct username');
